5b73ac73e7fc5b0c792a456d3304c1ee78cdc254
Dr-IceCream <90258388+Dr-IceCream@users.noreply.github.com>
2022-08-14 19:46:06 +0800

    Add files via upload

---
+++ lab6-cow/kernel/kalloc.c
@@ -0,0 +1,167 @@
+// Physical memory allocator, for user processes,
+// kernel stacks, page-table pages,
+// and pipe buffers. Allocates whole 4096-byte pages.
+
+#include "types.h"
+#include "param.h"
+#include "memlayout.h"
+#include "spinlock.h"
+#include "riscv.h"
+#include "defs.h"
+
+void freerange(void *pa_start, void *pa_end);
+
+extern char end[]; // first address after kernel.
+                   // defined by kernel.ld.
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct spinlock lock;
+  struct run *freelist;
+} kmem;
+
+struct {
+  struct spinlock lock;    //ensure the consistency
+  int cnt[PHYSTOP/PGSIZE]; //record the reference count
+}refcnt;
+
+void
+kinit()
+{
+  initlock(&kmem.lock, "kmem");
+  initlock(&refcnt.lock, "kref");
+  freerange(end, (void*)PHYSTOP);
+}
+
+void
+freerange(void *pa_start, void *pa_end)
+{
+  char *p;
+  p = (char*)PGROUNDUP((uint64)pa_start);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    acquire(&refcnt.lock);
+    refcnt.cnt[(uint64)p / PGSIZE] = 1; //
+    release(&refcnt.lock);
+    kfree(p);
+  }
+}
+
+// Free the page of physical memory pointed at by v,
+// which normally should have been returned by a
+// call to kalloc().  (The exception is when
+// initializing the allocator; see kinit above.)
+void
+kfree(void *pa)
+{
+  struct run *r;
+
+  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+    panic("kfree");
+
+  acquire(&refcnt.lock);
+  if(--refcnt.cnt[(uint64)pa/PGSIZE]==0){
+    release(&refcnt.lock);
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
+
+    r = (struct run*)pa;
+
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
+  }else{
+    release(&refcnt.lock);
+  }
+}
+
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
+void *
+kalloc(void)
+{
+  struct run *r;
+
+  acquire(&kmem.lock);
+  r = kmem.freelist;
+  if(r)
+    kmem.freelist = r->next;
+  release(&kmem.lock);
+
+  addref((char*)r);
+
+  if(r)
+    memset((char*)r, 5, PGSIZE); // fill with junk
+  return (void*)r;
+}
+
+//add the reference count with the spinlock
+int
+addref(void* pa)
+{
+  if( (uint64)pa%PGSIZE!=0 || (char*)pa < end || (uint64)pa > PHYSTOP){
+    return -1;
+  }
+  acquire(&refcnt.lock);
+  ++refcnt.cnt[(uint64)pa/PGSIZE];
+  release(&refcnt.lock);
+  return 0;
+}
+int 
+getcnt(void* pa)
+{
+  return refcnt.cnt[(uint64)pa / PGSIZE];
+}
+
+// check whether it is cow page
+// if it is ,return 0,else -1
+int
+cowpage(pagetable_t pagetable, uint64 va)
+{
+  if(va > MAXVA) return -1;
+  pte_t *pte = walk(pagetable, va, 0);
+  if(pte == 0) return -1;
+  if((*pte&PTE_V) == 0) return -1;
+  return (*pte & PTE_COW) ? 0 : -1; 
+}
+// alloc physical page for cow page
+void*
+cow_alloc(pagetable_t pagetable, uint64 va)
+{
+  if(va % PGSIZE != 0){
+    return 0;
+  }
+  uint64 pa = walkaddr(pagetable, va);
+  if(pa == 0){
+    return 0;
+  }
+
+  pte_t *pte = walk(pagetable, va, 0);
+  if(getcnt((char*)pa) == 1){
+    *pte |= PTE_W;
+    *pte &= ~PTE_COW;
+    return (void*)pa;
+  }else{ // there are more than one reference
+    char* mem;
+    if((mem=kalloc()) == 0){
+      return 0;
+     }
+    *pte &= ~PTE_V;
+    // copy the content to this new mem
+    memmove(mem, (char*)pa, PGSIZE);
+    // map and set the pte
+    if(mappages(pagetable, va, PGSIZE, (uint64)mem, (PTE_FLAGS(*pte) | PTE_W) & ~PTE_COW) != 0){
+      kfree(mem);
+      *pte &= ~PTE_V;
+      return 0;
+    }
+    // old physical page reference count--
+    kfree((char*)PGROUNDDOWN(pa));
+    return mem;
+  }
+}
+
